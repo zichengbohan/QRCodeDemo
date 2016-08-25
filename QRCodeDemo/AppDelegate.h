a6231f0aa61553e85494a0b90c9ae0b2b47cf4f5
于洪超 <xbmld1@gmail.com>
2016-08-25 14:48:18 +0800

    Initial Commit

---
+++ QRCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCodeDemo
+//
+//  Created by 胥佰淼 on 16/8/25.
+//  Copyright © 2016年 胥佰淼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
